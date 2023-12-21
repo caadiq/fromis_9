bbeb57866a99d10de3e50589c12b0e4d8fd38f3b
caadiq <caadiq@gmail.com>
2023-12-21 11:14:31 +0900

    뒤로가기 두 번 눌러서 앱 종료

--- app/src/main/java/com/beemer/unofficial/fromis_9/ActivityMain.kt
+++ app/src/main/java/com/beemer/unofficial/fromis_9/ActivityMain.kt
@@ -4,21 +4,36 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.beemer.unofficial.fromis_9.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ActivityMain : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     private val toolbar by lazy { binding.toolbar }
-    private val layoutFragment by lazy { binding.layoutFragment }
     private val bottombar by lazy { binding.bottombar }
+    private val layoutParent by lazy { binding.layoutParent }
+    private val layoutFragment by lazy { binding.layoutFragment }
 
     private var fragmentHome: FragmentHome? = null
     private var fragmentVideo: FragmentVideo? = null
     private var fragmentSchedule: FragmentSchedule? = null
+
+    private var backPressedTime: Long = 0
+    private val backPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (System.currentTimeMillis() - backPressedTime >= 2000) {
+                backPressedTime = System.currentTimeMillis()
+                Snackbar.make(layoutParent, getString(R.string.str_activity_main_press_back), 2000).show()
+            } else {
+                finish()
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +56,8 @@ class ActivityMain : AppCompatActivity() {
                 return true
             }
         })
+
+        onBackPressedDispatcher.addCallback(this, backPressedCallback)
 
         changeFragment(FragmentHome())
         bottombar.onItemSelected = ::onBottomBarItemSelected
