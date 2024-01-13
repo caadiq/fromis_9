21d487113317522e975d657a2515199a4d148144
caadiq <caadiq@gmail.com>
2024-01-13 17:36:38 +0900

    라이선스 목록 추가

--- app/src/main/java/com/beemer/unofficial/fromis_9/view/ActivitySettings.kt
+++ app/src/main/java/com/beemer/unofficial/fromis_9/view/ActivitySettings.kt
@@ -3,6 +3,7 @@ package com.beemer.unofficial.fromis_9.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.beemer.unofficial.fromis_9.databinding.ActivitySettingsBinding
+import com.mikepenz.aboutlibraries.LibsBuilder
 
 class ActivitySettings : AppCompatActivity() {
     private val binding by lazy { ActivitySettingsBinding.inflate(layoutInflater) }
@@ -12,5 +13,12 @@ class ActivitySettings : AppCompatActivity() {
         setContentView(binding.root)
 
 //        binding.switchDarkMode.isChecked = true
+        binding.txtLicenseDetails.setOnClickListener {
+            LibsBuilder()
+                .withAutoDetect(true)
+                .withLicenseShown(true)
+                .withAboutIconShown(true)
+                .start(this)
+        }
     }
 }
